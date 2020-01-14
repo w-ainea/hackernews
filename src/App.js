eafe2ddba9b6041a517b51d5e47bd53bdc56d56d
Awabwoba <aineah28@gmail.com>
2020-01-14 14:01:26 +0300

    added styling for the components

--- src/App.js
+++ src/App.js
@@ -58,61 +58,56 @@ class App extends Component {
   render() {
     // destructure the properties of state
     const { list, searchTerm } = this.state;
-
     return (
-      <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange}>
-          Search
-        </Search>
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange}>
+            Search
+          </Search>
+          <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+        </div>
       </div>
     );
   }
 }
 
-class Search extends Component {
-  render() {
-    // destructure the props that have been passed down from the parent component
-    const { value, onChange, children } = this.props;
-    return (
-      <form>
-        {children} <input type="text" value={value} onChange={onChange} />
-      </form>
-    );
-  }
-}
+const Search = ({ value, onChange, children }) => (
+  <form>
+    {children} <input type="text" value={value} onChange={onChange} />
+  </form>
+);
 
-class Table extends Component {
-  render() {
-    // destructure the props that have been passed down from the parent component
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => {
-          /* const onHandleDismiss=()=> this.onDismiss(item.objectID); */
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href="{item.url}"> {item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-              <span>
-                <button
-                  /* return a function that gets executed every time the button is clicked */
-                  onClick={() => onDismiss(item.objectID)}
-                  type="button"
-                >
-                  Dismiss
-                </button>
-              </span>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-}
+const Table = ({ list, pattern, onDismiss }) => (
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item => {
+      /* const onHandleDismiss=()=> this.onDismiss(item.objectID); */
+      return (
+        <div key={item.objectID} className="table-row">
+          <span>
+            <a href="{item.url}"> {item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <Button
+              /* return a function that gets executed every time the button is clicked */
+              onClick={() => onDismiss(item.objectID)}
+              className="button-inline"
+            >
+              Dismiss
+            </Button>
+          </span>
+        </div>
+      );
+    })}
+  </div>
+);
+
+const Button = ({ onClick, className = "", children }) => (
+  <button onClick={onClick} className={className} type="button">
+    {children}
+  </button>
+);
 
 export default App;
