0cff9fb2731906ea45a8c0c58424a66c362684b3
Awabwoba <aineah28@gmail.com>
2020-01-14 11:30:55 +0300

    added an input form and implemented the filter functionality

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ const list = [
   }
 ];
 
+// filter functionality
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
 class App extends Component {
   // the constructor is used to initialise local component  state
   constructor(props) {
@@ -28,33 +32,44 @@ class App extends Component {
 
     // state of the component
     this.state = {
-      list
+      list,
+      searchTerm: ""
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   // function to remove items from the list when the dismiss button is clicked
   onDismiss(id) {
     // function to determine if the id of the clicked item matched that of the item on the list
     const isNotId = item => item.objectID !== id;
-
     // update the list depending on the returned value from the isNotId function
     const updatedList = this.state.list.filter(isNotId);
     // update the state of the component
     this.setState({ list: updatedList });
   }
+  // function to filter the output based on what the user inputs into the form
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
 
   // the render method runs everytime state of the component changes in order to update the view
   render() {
-    // destructure the list property of state
-    const { list } = this.state;
+    // destructure the properties of state
+    const { list, searchTerm } = this.state;
 
     return (
       <div className="App">
-        {list.map(item => {
+        <form>
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item => {
           /* const onHandleDismiss=()=> this.onDismiss(item.objectID); */
-
           return (
             <div key={item.objectID}>
               <span>
